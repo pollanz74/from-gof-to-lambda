5fae571be9c1c0705d3a1000a01bcd2e9eec3c23
Andrea Pollani <andrea.pollani@nttdata.com>
2016-12-21 17:43:21 +0100

    watching talk...

--- src/main/java/org/mfusco/fromgoftolambda/talk/command/CommandLambda.java
+++ src/main/java/org/mfusco/fromgoftolambda/talk/command/CommandLambda.java
@@ -1,8 +1,40 @@
 package org.mfusco.fromgoftolambda.talk.command;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CommandLambda {
 
+	public static void log(String message) {
+		System.out.println("Logging: " + message);
+	}
+	
+	public static void save(String message) {
+		System.out.println("Saving: " + message);
+	}
+	
+	public static void send(String message) {
+		System.out.println("Sending: " + message);
+	}
+	
+    public static class Executor {
+        public void execute(List<Runnable> tasks) {
+            // method references call
+        	tasks.forEach(Runnable::run);
+            
+            // tasks.forEach(r -> r.run());
+        }
+    }
+	
     public static void main( String[] args ) {
-
+    	// Runnable oppure una qualsiasi interfaccia
+    	// con un unico metodo con signature compatibile
+    	// anche l'interfaccia Runnable di prima
+    	List<Runnable> tasks = new ArrayList<>();
+    	tasks.add(() -> log("Hi"));
+    	tasks.add(() -> save("Cheers"));
+    	tasks.add(() -> send("Bye"));
+    	
+    	new Executor().execute(tasks);
     }
 }
--- src/main/java/org/mfusco/fromgoftolambda/talk/decorator/DecoratorLambda.java
+++ src/main/java/org/mfusco/fromgoftolambda/talk/decorator/DecoratorLambda.java
@@ -1,8 +1,32 @@
 package org.mfusco.fromgoftolambda.talk.decorator;
 
+import java.util.function.DoubleUnaryOperator;
+import java.util.stream.Stream;
+
 public class DecoratorLambda {
 
-    public static void main( String[] args ) {
+	public static class DefaultSalaryCalculator implements DoubleUnaryOperator {
 
-    }
+		@Override
+		public double applyAsDouble(double grossAnnual) {
+			return grossAnnual / 12;
+		}
+
+	}
+
+	public static double calculate(double gross, DoubleUnaryOperator... fs) {
+		return Stream.of(fs) //
+				.reduce(DoubleUnaryOperator.identity(), DoubleUnaryOperator::andThen) //
+				.applyAsDouble(gross);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new DefaultSalaryCalculator() //
+				.andThen(Taxes::generalTax) //
+				.andThen(Taxes::regionalTax) //
+				.andThen(Taxes::healthInsurance) //
+				.applyAsDouble(30000.00));
+		
+		System.out.println(calculate(30000.00, new DefaultSalaryCalculator(), Taxes::generalTax, Taxes::regionalTax, Taxes::healthInsurance));
+	}
 }
